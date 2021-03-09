540a7506e94611c01262379cff65dcfb96f30c4b
Benjamin Rose <rose.benjamin@t-online.de>
2021-03-09 12:06:48 +0100

    Created character need and satisfaction system base

---
+++ Brackeys-StrongerTogether/Assets/Scripts/Core/CharacterComponents/AISatisfaction.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AISatisfaction : MonoBehaviour
+{
+    [SerializeField] Slider satisfactionSlider;
+
+    private float currentSatisfaction = 50f;
+
+
+    public void ChangeSatisfactionBy(float changeValue)
+    {
+        currentSatisfaction += changeValue;
+
+        if(currentSatisfaction <= 0f)
+        {
+            currentSatisfaction = 0f;
+        }
+
+        if(currentSatisfaction >= 100f)
+        {
+            currentSatisfaction = 100f;
+        }
+
+        UpdateSatisfactionMeter();
+    }
+
+    private void UpdateSatisfactionMeter()
+    {
+        satisfactionSlider.value = currentSatisfaction;
+    }
+}
